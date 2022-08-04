0118a1f9edf6e2a7cf2eea1a1e6cfda99eac784e
Gisele <gisesonia@gmail.com>
2022-08-04 09:56:22 -0300

    criando a entidade agencia

--- twobank/src/main/java/com/accenture/academico/model/Agencia.java
+++ twobank/src/main/java/com/accenture/academico/model/Agencia.java
@@ -1,5 +1,59 @@
 package com.accenture.academico.model;
 
-public class Agencia {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
 
+@Entity
+public class Agencia implements Serializable {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long idAgencia;
+
+    private Long numeroAgencia;
+
+    private String nomeAgencia;
+
+    private String endereco;
+
+    public Long getIdAgencia() {
+        return idAgencia;
+    }
+
+    public void setIdAgencia(Long idAgencia) {
+        this.idAgencia = idAgencia;
+    }
+
+    public Long getNumeroAgencia() {
+        return numeroAgencia;
+    }
+
+    public void setNumeroAgencia(Long numeroAgencia) {
+        this.numeroAgencia = numeroAgencia;
+    }
+
+    public String getNomeSgencia() {
+        return nomeAgencia;
+    }
+
+    public void setNomeSgencia(String nomeSgencia) {
+        this.nomeAgencia = nomeSgencia;
+    }
+
+    @Override
+    public String toString() {
+        return "Agencia{" +
+                "idAgencia=" + idAgencia +
+                ", numeroAgencia=" + numeroAgencia +
+                ", nomeSgencia='" + nomeAgencia + '\'' +
+                ", endereco=" + endereco +
+                '}';
+    }
 }
